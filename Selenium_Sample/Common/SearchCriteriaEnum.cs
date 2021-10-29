c0d3946a8bd3e2bb8a76c967b8630b09c32acf5e
Andrzej Owczarek <owczareka@tt.com.pl>
2021-10-29 10:33:09 +0200

    Added project selenium based for UI tests

--- Selenium_Sample/Common/SearchCriteriaEnum.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Selenium_Sample.Common
-{
-    public enum SearchCriteriaEnum
-    {
-        XPath,
-        Id,
-        CssSelector
-    }
-}
--- Selenium_Sample/Models/TestPageModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Selenium_Sample.Models
-{
-    class TestPageModel
-    {
-        public const string loginButtonXpath = "//a[@class='login']";
-    }
-}
